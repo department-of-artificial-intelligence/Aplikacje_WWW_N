e9dbe7669bcd9f01253a158b53bed1108a5b58de
Patryk Najgebauer <email@smth.smth>
2022-02-24 16:47:38 +0100

    ddd

---
+++ SchoolRegister.Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SchoolRegister.Model
+{
+    public class User : IdentityUser<int> 
+    {
+        public string FristName {get;set;}
+        public string LastName {get;set;}
+        public DateTime RegistrationDate {get;set;}
+    
+    }
+}
