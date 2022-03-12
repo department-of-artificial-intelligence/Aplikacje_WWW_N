f0a4f76bececed7305c0bba832fa8201aa517562
michalnaskrecki <micha.naskrecki@gmail.com>
2022-03-12 18:31:12 +0530

    Skonczone Lab1

---
+++ SchoolRegister.Model/DataModels/RoleValue.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public enum RoleValue
+    {
+       User = 1, 
+       Student = 2,
+       Parent = 3,
+       Teacher = 4,
+       Admin = 5 
+    }
+}
